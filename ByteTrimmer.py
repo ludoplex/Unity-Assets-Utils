f02749c9791e855c38a72aa1facdb266861dfa51
Haoming <hmstudy02@gmail.com>
2023-02-04 22:15:52 +0800

    Ver 2.0
    Now the Mode is automatically detected~

--- ByteTrimmer.py
+++ ByteTrimmer.py
@@ -1,13 +1,7 @@
 import sys
 import os
 
-if len(sys.argv) < 2:
-    sys.exit('Please specify the Mode')
-
-mode = sys.argv[1]
-delete = True if len(sys.argv) < 3 else sys.argv[2]
-bufsize = 420 if len(sys.argv) < 4 else sys.argv[3]
-cap = 0 if len(sys.argv) < 5 else sys.argv[4]
+delete = True if len(sys.argv) < 2 else sys.argv[1]
 
 currentPath = os.path.dirname(os.path.abspath(__file__))
 
@@ -16,38 +10,21 @@ for file in os.listdir(currentPath):
         continue
 
     with open(os.path.join(currentPath, file), 'rb') as in_file:
-        
-        if mode == 'single':
+        secondPass = False
 
-            for i in range(bufsize):
-                in_file.seek(i)
-                byte = in_file.read(5)
+        for i in range(1024):
+            in_file.seek(i)
+            byte = in_file.read(7)
 
-                if byte == b'Unity':
-                    pos = in_file.tell() - 5
+            if byte == b'UnityFS':
+                if not secondPass:
+                    pos = in_file.tell() - 7
+                    secondPass = True
+                else:
+                    pos = in_file.tell() - 7
                     break
 
-        elif mode == 'double':
-
-            secondPass = False
-            for i in range(bufsize):
-                in_file.seek(i)
-                byte = in_file.read(5)
-
-                if byte == b'Unity':
-                    if not secondPass:
-                        secondPass = True
-                    else:
-                        pos = in_file.tell() - 5
-                        break
-
-        else:
-            sys.exit('Mode = single | double')
-
-        if not cap == 0:
-            output = os.path.join(currentPath, file[:cap])
-        else:
-            output = os.path.join(currentPath, file) + '_edited'
+        output = os.path.join(currentPath, file) + '_edited'
 
         with open(output, 'wb+') as out_file:
             in_file.seek(0)
--- README.md
+++ README.md
@@ -1,12 +1,8 @@
 # AssetByteTrimmer
 ## How To Use
 - Put the script in the same folder as the assets
-- Run the script in command line followed by the mode: `single` or `double` **(required)**
-  - `single` is for files with only one Unity header after a few random leading bytes
-  - `double` is for files with two Unity header that is only readable after removing all the bytes before the second header
+- Run the script 
 - You can also specify if the old files should be deleted **(optional)**
-- You can also specify how long the script should try to search **(optional)**
-- You can also specify if the length of the edited file name should be capped **(optional)**
 ## Notice
-- Highly suggest you to read the code first. I'm not responsible for whatever happens to your files.
+- I'm not responsible for whatever happens to your files.
 - Old files are **deleted** by default.
